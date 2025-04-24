049595a794b03354836edbd78bbc78b811ebc20c
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-24 11:13:49 -0600

    try to fix vercel

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,13 +7,13 @@ if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not defined in the environment variables");
 }
 
-const db = new Sequelize(process.env.DATABASE_URL!, {
-
-	models: [__dirname + '/../models'],
-
-
-}
-);
+const db = new Sequelize(process.env.DATABASE_URL!, { 
+    models: [__dirname + "/../models/**/*"],
+    logging: false, 
+    dialect: 'postgres',
+    dialectModule: require('pg')
+  
+}) 
 
 export default db
 
